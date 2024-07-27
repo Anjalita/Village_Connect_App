bdb609525cb63044e88bbce12fdc8571c7d050fa
Ebey Joe Regi <ebeyjoeregi13@gmail.com>
2024-07-27 09:28:39 +0530

    v6

--- lib/screens/admin_home_screen.dart
+++ lib/screens/admin_home_screen.dart
@@ -5,8 +5,7 @@ import 'dart:convert';
 import 'admin_announcements_screen.dart';
 import 'admin_enquiry_screen.dart';
 import 'admin_suggestion_screen.dart';
-import 'pending_user_details_screen.dart';
-import 'Admin_Console_screen.dart'; // Import your AdminConsolePage
+import 'admin_console_screen.dart'; // Import your AdminConsolePage
 
 class AdminHomeScreen extends StatefulWidget {
   final String username;
@@ -90,56 +89,148 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
     _pageController.jumpToPage(index);
   }
 
+  void _showUserDetails(Map<String, dynamic> user) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Pending User Details'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Username: ${user['username']}',
+                    style: TextStyle(fontSize: 18)),
+                SizedBox(height: 8),
+                Text('Name: ${user['name']}', style: TextStyle(fontSize: 18)),
+                SizedBox(height: 8),
+                Text('Phone: ${user['phone']}', style: TextStyle(fontSize: 18)),
+                SizedBox(height: 8),
+                Text('Email: ${user['email']}', style: TextStyle(fontSize: 18)),
+                SizedBox(height: 8),
+                Text('Address: ${user['address']}',
+                    style: TextStyle(fontSize: 18)),
+                SizedBox(height: 8),
+                Text('Occupation: ${user['job_title']}',
+                    style: TextStyle(fontSize: 18)),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context); // Close the dialog
+                          _updateUserActivation(user['id'], true);
+                        },
+                        child: Text('Accept'),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context); // Close the dialog
+                          _updateUserActivation(user['id'], false);
+                        },
+                        child: Text('Reject'),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _selectedIndex == 0
-          ? AppBar(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Village Connect Admin'),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.admin_panel_settings,
-                            color: Colors.black),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  AdminConsolePage(), // Navigate to the Admin Console Page
-                            ),
-                          );
-                        },
+      body: Stack(
+        children: [
+          // Background image with opacity
+          Opacity(
+            opacity: 0.5, // Adjust the opacity value as needed
+            child: Image.asset(
+              'assets/images/bg1.png',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          // Main content
+          Column(
+            children: [
+              // AppBar
+              _selectedIndex == 0
+                  ? AppBar(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Village Connect Admin'),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.admin_panel_settings,
+                                    color: Colors.black),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          AdminConsolePage(), // Navigate to the Admin Console Page
+                                    ),
+                                  );
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.logout, color: Colors.black),
+                                onPressed: () {
+                                  Navigator.pushReplacementNamed(context, '/');
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.logout, color: Colors.black),
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/');
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+                      backgroundColor:
+                          Colors.transparent, // Set background to transparent
+                      elevation: 0, // Remove shadow
+                      automaticallyImplyLeading: false, // Disable back button
+                    )
+                  : Container(),
+              Expanded(
+                child: PageView(
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                  },
+                  children: <Widget>[
+                    _buildPendingUsersPage(),
+                    AdminAnnouncementPage(),
+                    AdminEnquiryScreen(),
+                    AdminSuggestionsScreen(),
+                  ],
+                ),
               ),
-              backgroundColor: Colors.teal,
-              automaticallyImplyLeading: false, // Disable back button
-            )
-          : null,
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        children: <Widget>[
-          _buildPendingUsersPage(),
-          AdminAnnouncementPage(),
-          AdminEnquiryScreen(),
-          AdminSuggestionsScreen(),
+            ],
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -171,58 +262,60 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
 
   Widget _buildPendingUsersPage() {
     return Scaffold(
+      backgroundColor:
+          Colors.transparent, // Ensure the background is transparent
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
           : _errorMessage.isNotEmpty
               ? Center(child: Text(_errorMessage))
-              : Column(
-                  children: [
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: _pendingUsers.length,
-                        itemBuilder: (context, index) {
-                          final user = _pendingUsers[index];
-                          return Card(
-                            margin: EdgeInsets.symmetric(vertical: 8.0),
-                            child: ListTile(
-                              contentPadding: EdgeInsets.all(16.0),
-                              title: Text(user['name']),
-                              subtitle:
-                                  Text('Occupation: ${user['job_title']}'),
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        PendingUserDetailsScreen(user: user),
-                                  ),
-                                ).then((_) =>
-                                    _fetchPendingUsers()); // Refresh on return
-                              },
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                    icon:
-                                        Icon(Icons.check, color: Colors.green),
-                                    onPressed: () {
-                                      _updateUserActivation(user['id'], true);
-                                    },
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.close, color: Colors.red),
-                                    onPressed: () {
-                                      _updateUserActivation(user['id'], false);
-                                    },
-                                  ),
-                                ],
+              : Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0), // Add padding here
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: _pendingUsers.length,
+                          itemBuilder: (context, index) {
+                            final user = _pendingUsers[index];
+                            return Card(
+                              margin: EdgeInsets.symmetric(vertical: 8.0),
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(16.0),
+                                title: Text(user['name']),
+                                subtitle:
+                                    Text('Occupation: ${user['job_title']}'),
+                                onTap: () {
+                                  _showUserDetails(
+                                      user); // Show the details in a dialog
+                                },
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(Icons.check,
+                                          color: Colors.green),
+                                      onPressed: () {
+                                        _updateUserActivation(user['id'], true);
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon:
+                                          Icon(Icons.close, color: Colors.red),
+                                      onPressed: () {
+                                        _updateUserActivation(
+                                            user['id'], false);
+                                      },
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
     );
   }
